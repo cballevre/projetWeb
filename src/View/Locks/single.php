56bc4d71961a5282e91d224e0797928d7759e442
Johann Carfantan <jojohann22550@gmail.com>
2018-06-14 18:00:02 +0200

    Locks

--- src/View/Locks/single.php
+++ src/View/Locks/single.php
@@ -5,3 +5,29 @@
  * Date: 12/06/2018
  * Time: 21:54
  */
+
+$doors = $array['doors'];
+$lock = $array['lock'];
+
+?>
+<section>
+    <h4>Portes associées</h4>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-striped">
+                <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($doors as $door): ?>
+                            <tr>
+                                <td><?php echo $door->getId(); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+            </table>
+        </div>
+    </div>
+</section>
