88cfa8dfced464194f4029aed02106ceb6bd91ec
ba <aliouneba2510@gmail.com>
2018-06-25 15:51:59 +0200

    Modification de

--- src/View/Doors/store.php
+++ src/View/Doors/store.php
@@ -10,7 +10,7 @@
 <div class="form-group row">
     <label class="col-sm-2 form-control-label text-xs-right"> Id du barillet : </label>
     <div class="col-sm-10">
-        <select class="custom-select" id="idLock" name="idLock" >
+        <select class="custom-select" id="idLock" name="idLock" required>
             <?php foreach($locks as $lock){?>
                 <option value="<?php echo $lock->getId(); ?>"><?php echo $lock->getId(); ?></option>
             <?php }?>
