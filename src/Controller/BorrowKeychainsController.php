b2873bdbeae24ceac8b3566eec0d70eacd77eec0
cballevre <celestin.ballevre@gmail.com>
2018-06-25 12:01:59 +0200

    Emprunter un trouseau

--- src/Controller/BorrowKeychainsController.php
+++ src/Controller/BorrowKeychainsController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 
 use App\Model\BorrowKeychain;
+use App\Model\KeyAssociation;
 use App\Model\Keychain;
 use Core\Repositories\RepositoryFactory;
 
@@ -42,19 +43,71 @@ class BorrowKeychainsController extends AppController
     public function store() {
 
         if(!empty($this->request->data)) {
+            var_dump($this->request->data);
 
             $borrowKeychain = new BorrowKeychain();
-            $borrowKeychain->setIdKeychain($this->request->data->idKeychain);
             $borrowKeychain->setIdUser($this->request->data->idUser);
-            $borrowKeychain->setDateRetour(\DateTime::createFromFormat("Y-m-d\TH:i", $this->request->data->dateRetour));
+
+            $date_retour = \DateTime::createFromFormat("Y-m-d\TH:i", $this->request->data->dateRetour);
+            $date_now = new \DateTime();
+            $keychainsModel = RepositoryFactory::getRepository('keychains');
+            $keychain = new Keychain();
+
+            $keychain->setCreationDate($date_now);
+            $keychain->setDestructionDate($date_retour);
+
+            $keychain = $keychainsModel->create(array($keychain))[0];
+            $keys = array();
+            $keyAssociations = array();
+
+            $keyAssociationModel = RepositoryFactory::getRepository('keyAssociations');
+            $keyModel = RepositoryFactory::getRepository('keys');
+
+            foreach ($this->request->data->keys as $key_id) {
+
+                var_dump($key_id);
+
+                $key = $keyModel->findById($key_id);
+                $key->setEtat("Emprunter");
+                $keyModel->update($key, $key->getId());
+
+                $keyAssociation = new KeyAssociation();
+                $keyAssociation->setIdKey(intval($key_id));
+                $keyAssociation->setIdKeychain($keychain->getId());
+                array_push($keyAssociations, $keyAssociation);
+
+            }
+
+            $keyAssociationModel->create($keyAssociations);
+
+            $borrowKeychain->setIdKeychain($keychain->getId());
+            $borrowKeychain->setDateRetour($date_retour);
 
             $model = RepositoryFactory::getRepository('borrowKeychains');
             $model->create(array($borrowKeychain));
+
+            $this->flash->set("L'emprunt a bien etait effectué ;)", "success");
 
             $this->redirect(WEBROOT . "?controller=borrowKeychains&action=index");
 
 
         } else {
+
+            $usersModel = RepositoryFactory::getRepository('users');
+            $users = $usersModel->findAll();
+
+            $keysModel = RepositoryFactory::getRepository('keys');
+            $keys_preresult = $keysModel->findAll();
+
+            $keys = array();
+
+            foreach ($keys_preresult as $key) {
+                if($key->getEtat() == "Disponible") {
+                    array_push($keys, $key);
+                }
+            }
+
+            $this->set(compact('users', 'keys'));
             $this->renderWithoutLayout('store');
         }
 
